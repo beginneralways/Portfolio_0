8d60d1bd537f1cf460330d3bddfb39fdf25a25e4
Prashant Chaudhary <donem7510@gmail.com>
2024-01-31 14:19:56 +0545

    added_react_router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import './App.css';
-import Header from './components/Header.js'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, BrowserRouter } from 'react-router-dom';
+import Header from './components/Header.js';
 import Skills from './components/Skills.js';
 import Stack from './components/Stack.js';
 
 function App() {
   return (
+    <BrowserRouter>
+    <Router>
       <div style={appStyle}>
-      <Header />
-      <Skills />
-      <Stack/>
-    
-    </div>
+        <Header />
+        <Switch>
+          <Route path="/skills" component={Skills} />
+          <Route path="/stack" component={Stack} />
+          {/* Add more routes as needed */}
+        </Switch>
+      </div>
+    </Router>
+    </BrowserRouter>
   );
 }
 
